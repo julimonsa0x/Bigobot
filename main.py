5f0066a0fedbe3bdbe00bd111990bf4ba344352b
julimonsa0x <jmonsa@outllok.com>
2021-06-03 20:05:52 -0300

    v0.2

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ bigo_guild_id = 559592087054450690  # if bot is public, call the var "base_guild
 bigo_guild_base = bot.fetch_guild(bigo_guild_id)  # if bot is public, call the var "base_guild"
 
 bigos = 559592087641915433
-general_bigos = bot.fetch_guild(bigos)
+general_bigos = bot.fetch_channel(bigos)
 
 # --------> Bot en marcha <-------
 @bot.event 
@@ -203,7 +203,7 @@ async def feliz_jueves():
         # if today IS thursday send message
         # and wait for a whole week to resend
         if today_int == 3:
-            await general_bigos.send("https://cdn.discordapp.com/attachments/793309880861458473/849848243662618644/Feliz_Jueves.mp4")
+            await general_bigos(f"**Feliz Jueves**\nhttps://cdn.discordapp.com/attachments/793309880861458473/849848243662618644/Feliz_Jueves.mp4")
             await asyncio.sleep(60 * 60 * 24 * 7)
 
 bot.loop.create_task(feliz_jueves())
