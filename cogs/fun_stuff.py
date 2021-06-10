eeb3c34ddbf81cad848be0772c62e44fdad0786a
julimonsa0x <jmonsa@outllok.com>
2021-06-10 01:29:41 -0300

    v0.2

--- cogs/fun_stuff.py
+++ cogs/fun_stuff.py
@@ -321,7 +321,7 @@ class FunCommands(commands.Cog):
 
         else:
             embedMeme = discord.Embed(color = discord.Colour.red())
-            embedMeme.set_image(url = random_link)
+            embedMeme.set_image(url = str(random_link))
             await ctx.send(content="meme salido del horno", embeds=[embedMeme])
 
 
